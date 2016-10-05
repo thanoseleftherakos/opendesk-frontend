b9958b25203e876cc055e6c1ba7aa0530c237030
Thanos Eleftherakos <man_or_astroman@MacBook-Air.local>
2016-10-05 23:16:16 +0300

    change menu layout

--- src/components/sidebar/menu.js
+++ src/components/sidebar/menu.js
@@ -2,32 +2,37 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { I18n } from 'react-redux-i18n';
 
-class Menu extends Component {
+class Menu2 extends Component {
 	render() {
 		return (
 			<div className="page-sidebar-wrapper">
                 <div className="page-sidebar navbar-collapse collapse">
-                    <ul className="page-sidebar-menu  page-header-fixed" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
-                        <li className="sidebar-toggler-wrapper hide">
-                            <div className="sidebar-toggler"> </div>
-                        </li>
-                        <li lassName="nav-item start" ></li>
+                    <ul className="page-sidebar-menu  page-header-fixed page-sidebar-menu-hover-submenu" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
                         <li className={'nav-item ' + (this.props.active == '/hotel/dashboard' ? 'active' : '') }>
                             <Link to={'/hotel/dashboard'} className="nav-link nav-toggle">
                                 <i className="icon-home"></i>
                                 <span className="title">Dashboard</span>
+                                {(this.props.active == '/hotel/dashboard') && 
+                                    <span className="selected"></span>
+                                }
                             </Link>
                         </li>
                         <li className={'nav-item ' + (this.props.active == '/hotel/reservations' ? 'active' : '') }>
                             <Link to={'/hotel/reservations'} className="nav-link nav-toggle">
                                 <i className="icon-calendar"></i>
                                 <span className="title">{I18n.t('general.reservations')}</span>
+                                {(this.props.active == '/hotel/reservations') && 
+                                    <span className="selected"></span>
+                                }
                             </Link>
                         </li>
                         <li className={'nav-item ' + (this.props.active == '/hotel/reservation/create' ? 'active' : '') }>
                             <Link to={'/hotel/reservation/create'} className="nav-link nav-toggle">
-                                <i className="icon-plus"></i>
+                                <i className="icon-calendar"></i>
                                 <span className="title">{I18n.t('general.new_reservation')}</span>
+                                {(this.props.active == '/hotel/reservation/create') && 
+                                    <span className="selected"></span>
+                                }
                             </Link>
                         </li>
                     </ul>
@@ -37,4 +42,4 @@ class Menu extends Component {
 	}
 }
 
-export default Menu;
+export default Menu2;
