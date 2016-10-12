07ed29064d45b7e748e1247426d579a1bf0187d3
Thanos Eleftherakos <teleftherakos@gmail.com>
2016-10-12 17:07:35 +0300

    change footer text

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,8 +4,7 @@ class Footer extends Component {
 	render() {
 		return (
 			<div className="page-footer">
-	            <div className="page-footer-inner"> 2014 &copy; Metronic by keenthemes.
-	                <a href="http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" title="Purchase Metronic just for 27$ and get lifetime updates for free" target="_blank">Purchase Metronic!</a>
+	            <div className="page-footer-inner"> 2016 &copy; wf8
 	            </div>
 	            <div className="scroll-to-top">
 	                <i className="icon-arrow-up"></i>
