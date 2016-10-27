5da569c8d425f5a4f18ec8636501da25d8a2f5ce
Thanos Eleftherakos <teleftherakos@gmail.com>
2016-10-27 17:15:42 +0300

    process.env to webpack prod

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -15,6 +15,9 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        }),
         new HtmlWebpackPlugin({
             template: 'index.html',
             inject: 'body',
